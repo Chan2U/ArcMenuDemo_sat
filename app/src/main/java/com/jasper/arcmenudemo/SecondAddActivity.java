ed8275eeb5f64ca0ab18b84252a6698f32d6fffa
Chan <chan2u@163.com>
2016-10-28 11:44:16 +0800

    activity add

---
+++ app/src/main/java/com/jasper/arcmenudemo/SecondAddActivity.java
@@ -0,0 +1,13 @@
+package com.jasper.arcmenudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_add);
+    }
+}
