804eea1960d7654a4deb0344b3ebbc61284640f4
Chan <Chan2U@163.com>
2016-10-28 15:51:00 +0800

    activity add forth

---
+++ app/src/main/java/com/jasper/arcmenudemo/ForthAddActivity.java
@@ -0,0 +1,13 @@
+package com.jasper.arcmenudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForthAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forth_add);
+    }
+}
